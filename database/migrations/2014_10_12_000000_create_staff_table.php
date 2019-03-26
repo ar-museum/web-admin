a6a450c55dbae091123c4c19d8e34c3f2b0e9dc3
Eugen <eugen.zaharia88@gmail.com>
2019-03-26 20:31:32 +0200

    #master - updates

--- database/migrations/2014_10_12_000000_create_staff_table.php
+++ database/migrations/2014_10_12_000000_create_staff_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStaffTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        Schema::create('staff', function (Blueprint $table) {
+            $table->increments('staff_id');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
