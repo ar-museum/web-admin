7364f5a233cf24461b1740ba506f1ae79599b0b7
Coța Octavian <45451082+cotaoctavian@users.noreply.github.com>
2019-03-30 21:28:08 +0200

    Create ExhibitController.php

---
+++ app/Http/Controllers/Web/ExhibitController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Controllers\Web;
+use App\Http\Controllers\Controller;
+
+class ExhibitController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function store($var){
+
+    }
+
+    public function edit($var){
+
+    }
+
+    public function delete($var){
+
+    }
+}
