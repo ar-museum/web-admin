94a6c31f1f5b9f888fdca6763d2696a37f7efa49
Andrei Chiperi <44604723+chiperiandrei@users.noreply.github.com>
2019-03-30 16:42:19 +0300

    Add files via upload

---
+++ database/migrations/2019_03_30_105937_exposition.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Exposition extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('exposition', function (Blueprint $table) {
+            $table->increments('exposition_id');
+            $table->string('title');
+            $table->string('description');
+            $table->unsignedInteger('staff_id');
+            $table->foreign('staff_id')
+                  ->references('staff_id')->on('staff')
+                  ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('exposition');
+    }
+}
