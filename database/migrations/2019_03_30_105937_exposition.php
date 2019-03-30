6afc8a4ac0da057e639ef9d75e7fc1317fcdd693
Andrei Chiperi <44604723+chiperiandrei@users.noreply.github.com>
2019-03-30 17:03:20 +0200

    add indexes

--- database/migrations/2019_03_30_105937_exposition.php
+++ database/migrations/2019_03_30_105937_exposition.php
@@ -22,6 +22,7 @@ class Exposition extends Migration
             $table->foreign('staff_id')
                   ->references('staff_id')->on('staff')
                   ->onDelete('cascade');
+            $table->unique('staff_id');
 
         });
     }
