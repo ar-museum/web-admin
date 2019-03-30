d5e8c4ec313bd52902430e84c08507caa2d06397
Coța Octavian <45451082+cotaoctavian@users.noreply.github.com>
2019-03-30 21:52:35 +0200

    Update ExhibitTableSeeder.php

--- database/seeds/ExhibitTableSeeder.php
+++ database/seeds/ExhibitTableSeeder.php
@@ -4,6 +4,11 @@ use Illuminate\Database\Seeder;
 
 class ExhibitTableSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
         DB::table('exhibit')->insert([
