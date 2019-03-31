0c4eb1777d65f29cde45f0893da67f7b60791e31
Băcăoanu Bianca <48392065+biancabacaoanu@users.noreply.github.com>
2019-03-31 12:25:23 +0300

    Update AuthorSeeder.php

--- database/seeds/AuthorSeeder.php
+++ database/seeds/AuthorSeeder.php
@@ -16,8 +16,8 @@ class AuthorSeeder extends Seeder
             'born_year' => '1850',
             'died_year' => '1889',
             'location' => 'Ipotesti',
-            'photo_id' => 1072,
-            'staff_id' =>5
+            'photo_id' => 1,
+            'staff_id' =>1
         ]);
 
     }
