5f1229eed586714d7b84d64eacd3a7cfd876ec3a
Deny - Constantin Pătrașcu <denypatrascu@gmail.com>
2019-03-31 12:39:25 +0300

    Migrations Category/Tag

---
+++ database/migrations/2019_03_31_000000_create_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag', function (Blueprint $table) {
+            $table->increments('tag_id');
+            $table->string('name');
+            $table->foreign('staff_id')
+                  ->references('staff_id')->on('exhibit')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag');
+    }
+}
