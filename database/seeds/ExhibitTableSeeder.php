e17ceabf86a32a64a904a68e363b5de13fdc6bd2
Coța Octavian <45451082+cotaoctavian@users.noreply.github.com>
2019-04-06 22:52:22 +0300

    Update ExhibitTableSeeder.php

--- database/seeds/ExhibitTableSeeder.php
+++ database/seeds/ExhibitTableSeeder.php
@@ -11,18 +11,6 @@ class ExhibitTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('exhibits')->insert([
-            'title' => 'Floarea albastra',
-            'short_description' => 'So deep!',
-            'description' => 'Cea mai splendida poezie ever!',
-            'start_year' => '1873',
-            'end_year' => '2019',
-            'size' => '20x30cm',
-            'location' => 'Iasi',
-            'author_id' => 1,
-            'exposition_id' => 1,
-            'staff_id' => 1
-        ]);
-
+        factory(App\Models\Exhibit::class, 5)->create();
     }
 }
