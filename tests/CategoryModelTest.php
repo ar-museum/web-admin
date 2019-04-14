5298421fea7ca38111e4e6a20eee208912bf61a1
Deny Patrascu <denypatrascu@gmail.com>
2019-04-14 20:35:02 +0300

    Add UnitTests: Category + Tag

---
+++ tests/CategoryModelTest.php
@@ -0,0 +1,119 @@
+<?php
+
+use App\Models\Category;
+
+class CategoryModelTest extends TestCase
+{
+    /**
+     * Test functions.
+     *
+     * @return void
+     */
+
+    public function testCreatingModel()
+    {
+        define('categoryId', 7);
+        define('categoryName', 'Poezie');
+        define('categoryStaffId', 1);
+
+        $category = factory(App\Models\Category::class)->make([
+            'category_id' => categoryId,
+            'name' => categoryName,
+            'staff_id' => categoryStaffId
+        ]);
+
+        // testing category_id
+        $this->assertNotEmpty(
+            $category->category_id,
+            'category_id is empty'
+        );
+        $this->assertTrue(
+            is_int($category->category_id),
+            'category_id is not an integer'
+        );
+        $this->assertSame(
+            categoryId,
+            $category->category_id,
+            'category_id has not been set properly'
+        );
+
+        // testing name
+        $this->assertNotEmpty(
+            $category->name,
+            'name is empty'
+        );
+        $this->assertTrue(
+            is_string($category->name),
+            'name is not an string'
+        );
+        $this->assertSame(
+            categoryName,
+            $category->name,
+            'name has not been set properly'
+        );
+
+        // testing staff_id
+        $this->assertNotEmpty(
+            $category->staff_id,
+            'staff_id is empty'
+        );
+        $this->assertTrue(
+            is_int($category->staff_id),
+            'staff_id is not an integer'
+        );
+        $this->assertSame(
+            categoryStaffId,
+            $category->staff_id,
+            'staff_id has not been set properly'
+        );
+    }
+
+    public function testExhibitRelationships()
+    {
+        $categories = factory(App\Models\Category::class)->create([
+            'name' => 'Roman',
+            'staff_id' => 1
+        ]);
+
+        $this->assertNull($categories->exhibit()->first());
+    }
+
+    public function testLastFive()
+    {
+        $tempCategories = factory(App\Models\Category::class, 5)->create([
+            'name' => 'Roman',
+            'staff_id' => 1
+        ]);
+
+        $tempCategories = $tempCategories->sortByDesc('category_id');
+
+        $stack = array();
+
+        for ($index = 4; $index >= 0; --$index)
+        {
+            array_push($stack, $tempCategories->offsetGet($index)->toArray());
+        }
+
+        $tempCategories = $stack;
+
+        $this->assertCount(5, $tempCategories);
+
+        $category = new Category();
+
+        $categories = $category->scopeLastFive()->get();
+
+        $this->assertEquals($tempCategories, $categories->toArray());
+    }
+
+    public function testFileExistence()
+    {
+        $this->assertFileExists('app/Models/Category.php');
+    }
+
+    public function testInstance()
+    {
+        $category = new Category();
+        $this->assertInstanceOf(Category::class, $category);
+    }
+}
+
---
+++ tests/TagModelTest.php
@@ -0,0 +1,117 @@
+<?php
+
+use App\Models\Tag;
+
+class TagModelTest extends TestCase
+{
+    /**
+     * Test functions.
+     *
+     * @return void
+     */
+    public function testCreatingModel()
+    {
+        define('tagId', 7);
+        define('tagName', 'romantism');
+        define('tagStaffId', 1);
+
+        $tag = factory(App\Models\Category::class)->make([
+            'tag_id' => tagId,
+            'name' => tagName,
+            'staff_id' => tagStaffId
+        ]);
+
+        // testing tag_id
+        $this->assertNotEmpty(
+            $tag->tag_id,
+            'tag_id is empty'
+        );
+        $this->assertTrue(
+            is_int($tag->tag_id),
+            'tag_id is not an integer'
+        );
+        $this->assertSame(
+            tagId,
+            $tag->tag_id,
+            'tag_id has not been set properly'
+        );
+
+        // testing name
+        $this->assertNotEmpty(
+            $tag->name,
+            'name is empty'
+        );
+        $this->assertTrue(
+            is_string($tag->name),
+            'name is not an string'
+        );
+        $this->assertSame(
+            tagName,
+            $tag->name,
+            'name has not been set properly'
+        );
+
+        // testing staff_id
+        $this->assertNotEmpty(
+            $tag->staff_id,
+            'staff_id is empty'
+        );
+        $this->assertTrue(
+            is_int($tag->staff_id),
+            'staff_id is not an integer'
+        );
+        $this->assertSame(
+            tagStaffId,
+            $tag->staff_id,
+            'staff_id has not been set properly'
+        );
+    }
+
+    public function testExhibitRelationships()
+    {
+        $tags = factory(App\Models\Tag::class)->create([
+            'name' => 'epigrame-noi',
+            'staff_id' => 1
+        ]);
+
+        $this->assertNull($tags->exhibit()->first());
+    }
+
+    public function testLastFive()
+    {
+        $tempTags = factory(App\Models\Tag::class, 5)->create([
+            'name' => 'cel-mai-citit',
+            'staff_id' => 1
+        ]);
+
+        $tempTags = $tempTags->sortByDesc('tag_id');
+
+        $stack = array();
+
+        for ($index = 4; $index >= 0; --$index)
+        {
+            array_push($stack, $tempTags->offsetGet($index)->toArray());
+        }
+
+        $tempTags = $stack;
+
+        $this->assertCount(5, $tempTags);
+
+        $tag = new Tag();
+
+        $tags = $tag->scopeLastFive()->get();
+
+        $this->assertEquals($tempTags, $tags->toArray());
+    }
+
+    public function testFileExistence()
+    {
+        $this->assertFileExists('app/Models/Tag.php');
+    }
+
+    public function testInstance()
+    {
+        $tag = new Tag();
+        $this->assertInstanceOf(Tag::class, $tag);
+    }
+}
