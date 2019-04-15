5786fd605330caa9736e79b35d8541c53480ce23
cosminiureanu <48418572+cosminiureanu@users.noreply.github.com>
2019-04-15 08:13:50 +0300

    Museum test

--- tests/MuseumModelTest.php
+++ tests/MuseumModelTest.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Models\Exhibit;
+use App\Models\Exposition;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class MuseumModelTest extends TestCase
 {
+
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -22,13 +26,31 @@ class MuseumModelTest extends TestCase
 
         ]);
         $this->assertGreaterThan(3,strlen($museum->name));
-        $this->assertGreaterThan(10,strlen($museum->address));
+        $this->assertGreaterThan(6,strlen($museum->address));
         $this->assertTrue(true,ctype_digit ( $museum->museum_id));
 
 
 
     }
 
+
+    public function testExpositionsRelationships()
+    {
+        $exposition = factory(App\Models\Exposition::class, 1)->create([
+            'title' => 'Tablouri mai putin cunoscute',
+            'description' => 'Tablouri ale unor artisti amatori locali',
+        ]);
+
+        $this->tempMuseum['museum_id'] = $exposition->museum_id;
+
+        $tempMuseum = factory(App\Models\Museum::class, 1);
+
+        $this->assertNotNull($tempMuseum->expositions()->first());
+
+        $this->assertInstanceOf(\Illuminate\Database\Eloquent\Collection::class, $tempMuseum->expositions()->get());;
+
+        $this->assertEquals($exposition->toArray(), $tempMuseum->expositions()->orderBy('exposition_id', 'desc')->first()->toArray());
+    }
 
     public function testExample()
     {
