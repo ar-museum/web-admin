0aa9420abbd03f6a8616226f3488fa195b181829
denypatrascu <denypatrascu@gmail.com>
2019-05-05 03:15:37 +0300

    Create: migration Vuforia

---
+++ database/migrations/2019_05_04_000000_create_vuforia_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVuforiaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vuforia', function(Blueprint $table) {
+            $table->increments('vuforia_id');
+
+            $table->unsignedInteger('museum_id');
+
+            $table->string('version', 5);
+
+            $table->unsignedInteger('file_id');
+            $table->foreign('file_id')
+                  ->references('media_id')->on('media')
+                  ->onDelete('cascade');
+
+            $table->enum('file_type', ['XML', 'DAT']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vuforia');
+    }
+}
