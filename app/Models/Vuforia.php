119077b66b7f53099d988d615142ed604186131a
denypatrascu <denypatrascu@gmail.com>
2019-05-05 03:15:58 +0300

    Create: model Vuforia

---
+++ app/Models/Vuforia.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Models;
+
+use App\BaseModel;
+use Illuminate\Notifications\Notifiable;
+
+class Vuforia extends BaseModel
+{
+    use Notifiable;
+
+    // Members
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'vuforia';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'vuforia_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'museum_id', 'version'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'file_id', 'file_type', 'created_at', 'updated_at'
+    ];
+
+    // Relationship methods
+
+    public function museum()
+    {
+        return $this->belongsTo(Museum::class, 'museum_id', 'museum_id');
+    }
+
+    public function media(){
+        return $this->belongsTo(Media::class, 'file_id', 'media_id');
+    }
+}
