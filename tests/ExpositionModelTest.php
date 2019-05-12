94ceee335899afb03418395a4f6ec1731e5f52eb
Andrei Chiperi <44604723+chiperiandrei@users.noreply.github.com>
2019-05-12 15:31:55 +0300

    Update ExpositionModelTest.php

--- tests/ExpositionModelTest.php
+++ tests/ExpositionModelTest.php
@@ -40,19 +40,49 @@ class ExpositionModelTest extends TestCase
     public function testCreatingModel()
     {
         $exposition = factory(App\Models\Exposition::class)->make($this->tempExposition);
-        $this->assertGreaterThan(3, strlen($exposition->title));
-        $this->assertGreaterThan(4, strlen($exposition->description));
-        $this->assertTrue(true, is_int($exposition->museum_id));
-        $this->assertTrue(true, is_int($exposition->staff_id));
+
         $this->assertTrue(true, is_int($exposition->photo_id));
 
 
     }
 
+    public function testTitleLength()
+    {
+        $exposition = factory(App\Models\Exposition::class)->make($this->tempExposition);
+        $this->assertGreaterThan(3, strlen($exposition->title));
+    }
+
+    public function testDescLength()
+    {
+        $exposition = factory(App\Models\Exposition::class)->make($this->tempExposition);
+        $this->assertGreaterThan(4, strlen($exposition->description));
+
+    }
+
+    public function testMuseumId()
+    {
+        $exposition = factory(App\Models\Exposition::class)->make($this->tempExposition);
+        $this->assertTrue(true, is_int($exposition->museum_id));
+
+    }
+
+    public function testStaffId()
+    {
+        $exposition = factory(App\Models\Exposition::class)->make($this->tempExposition);
+        $this->assertTrue(true, is_int($exposition->staff_id));
+
+    }
+
+
     public function testFailure()
     {
         $this->assertFileExists('app/Models/Exposition.php');
 
+
+    }
+
+    public function testRelationship()
+    {
         $this->assertInstanceOf(\App\Models\Exposition::class, new Exposition);
     }
 
@@ -70,13 +100,23 @@ class ExpositionModelTest extends TestCase
 
     public function testCustomAttributes()
     {
-        $this->assertSame('expositions', (new App\Models\Exposition())->getTable());
-        $this->assertSame('exposition_id', (new App\Models\Exposition())->getKeyName());
+
+
         $this->assertSame([
             'museum_id',
             'title',
             'description',
         ], (new App\Models\Exposition)->getFillable());
+    }
+
+    public function testTableIdentic()
+    {
+        $this->assertSame('expositions', (new App\Models\Exposition())->getTable());
+    }
+
+    public function testRowIdentic()
+    {
+        $this->assertSame('exposition_id', (new App\Models\Exposition())->getKeyName());
     }
 
 
@@ -86,30 +126,30 @@ class ExpositionModelTest extends TestCase
 
         $this->assertNull($exposition->photo()->first());
     }
-      public function testLastFive()
+
+    public function testLastFive()
     {
         $tempExpo = factory(App\Models\Exposition::class, 5)->create($this->tempExposition)->sortByDesc('exposition_id');
-         $stack = array();
-        for ($index = 4; $index >= 0; --$index)
-        {
+        $stack = array();
+        for ($index = 4; $index >= 0; --$index) {
             array_push($stack, $tempExpo->offsetGet($index)->toArray());
         }
-         $tempExpo = $stack;
-         $this->assertCount(5, $tempExpo);
-         $expositions = Exposition::scopeLastFive()->get();
-         $this->assertEquals($tempExpo, $expositions->toArray());
-     }
-    
-     public function testStaffRelationships()
+        $tempExpo = $stack;
+        $this->assertCount(5, $tempExpo);
+        $expositions = Exposition::scopeLastFive()->get();
+        $this->assertEquals($tempExpo, $expositions->toArray());
+    }
+
+    public function testStaffRelationships()
     {
         $tempExpo = factory(App\Models\Exposition::class, 1)->create($this->tempExposition);
         $this->assertNull($tempExpo->staff()->first());
         $exposition = Exposition::find(1);
         $staff = new App\Models\Staff([
-            'first_name'     => 'Vasile',
-            'last_name'      => 'Popescu',
-            'email'          => 'gigel@museum.lc',
-            'password'       => 'parola',
+            'first_name' => 'Vasile',
+            'last_name' => 'Popescu',
+            'email' => 'gigel@museum.lc',
+            'password' => 'parola',
             'remember_token' => '321',
         ]);
         $exposition->staff()->save($staff);
@@ -118,7 +158,6 @@ class ExpositionModelTest extends TestCase
         $this->assertCount(2, $staff->toArray());
         $this->assertEquals($staff->toArray(), $exposition->staff()->orderBy('staff_id', 'desc')->first()->toArray());
     }
-
 
 
 }
