714469daca706f3c1726ab6188fcb8346f28d0b2
Coța Octavian <45451082+cotaoctavian@users.noreply.github.com>
2019-05-15 12:06:36 +0300

    update UpdateController for Vuforia version.

--- app/Http/Controllers/Api/UpdateController.php
+++ app/Http/Controllers/Api/UpdateController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Dragndrop;
 use App\Models\Museum;
-use App\Models\VuforiaFile;
+use App\Models\Vuforia;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UpdateController extends Controller
@@ -13,43 +13,53 @@ class UpdateController extends Controller
     public function index()
     {
 
-        if(empty(request()->has("version"))){
+        if (empty(request()->has("version")) && empty(request()->has("museum_id"))) {
             return response()->json(array(
-                'mesage'      =>  "FORBIDDEN",
+                'mesage' => "FORBIDDEN",
             ), 403);
         }
 
         $version = request()->get("version");
+        $museum_id = request()->get('museum_id');
 
-        $vuf = VuforiaFile::select(['path'])
-            ->join('vuforia', 'vuforia.file_id', '=', 'vuforia_files.file_id')
+        $vuf = Vuforia::select(['vuforia_files.path' , 'vuforia.museum_id'])
+            ->join('vuforia_files', 'vuforia.file_id', '=', 'vuforia_files.file_id')
+            ->where('vuforia.museum_id', $museum_id)
             ->where('vuforia.version', $version)
             ->orderBy("version", "ASC")->get()->toArray();
 
-        if(count($vuf)) {
+
+        if (!count($vuf[0]['museum_id'])) {
+            return response()->json(array(
+                'message' => "Nu exista muzeul.",
+            ), 403);
+        }
+
+
+        if (count($vuf[0]['path'])) {
             $vuf[0]['path'] = str_replace('\\', '/', $vuf[0]['path']);
             $vuf[1]['path'] = str_replace('\\', '/', $vuf[1]['path']);
             $var['files'] = [$vuf[0]['path'], $vuf[1]['path']];
             unset($vuf['path']);
             return response()->json($var);
-        }
-        else return response()->json(array(
-            'message'      =>  "Nu exista versiunea.",
+        } else return response()->json(array(
+            'message' => "Nu exista versiunea.",
         ), 403);
     }
 
-    public function drag($id){
+    public function drag($id)
+    {
         try {
             $test = Museum::findOrFail($id);
         } catch (ModelNotFoundException $ex) {
             return response()->json(array(
-                'mesaj'   =>  'Nu exista acest muzeu in baza de date.'
+                'mesaj' => 'Nu exista acest muzeu in baza de date.'
             ), 404);
         }
 
         $museums = Dragndrop::where('museum_id', $id)->get();
 
-        foreach($museums as $var => $museum) {
+        foreach ($museums as $var => $museum) {
             unset($museum['museum_id']);
             unset($museum['dragndrop_id']);
             $museum['path'] = str_replace('\\', '/', $museum['path']);
